9596021ebf9c980454b4f9ddcf9b58435633e129
hidehic0 <hidehico.ooba@gmail.com>
2025-11-18 21:43:50 +0900

    引数パース

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+use std::process;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short)]
+    n: u32,
+    #[arg(short, default_value_t = 1)]
+    m: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.n >= 18 {
+        println!("this tool supports generating prime numbers up to 17 digits");
+        process::exit(1);
+    }
 }
