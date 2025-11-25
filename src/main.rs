32e012aacaf346f978bb70dc6bfd0da700eb72db
hidehic0 <hidehico.ooba@gmail.com>
2025-11-25 16:23:36 +0900

    バグが存在する素数判定関数

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,75 @@ fn create_number(d: u32, rng: &mut ThreadRng) -> i64 {
     return res;
 }
 
+fn mod_pow(mut a: i128, mut n: i128, m: i128) -> i128 {
+    let mut res = 1;
+
+    while n != 0 {
+        if n % 2 == 0 {
+            res = (res * a) % m;
+        }
+        a = (a * a) % m;
+        n >>= 1
+    }
+    return res;
+}
+
+fn check_prime(n: i128) -> bool {
+    if n == 2 {
+        return true;
+    } else if n == 1 || (n & 1) == 0 {
+        return false;
+    }
+
+    let mut s = 0;
+    let mut d = n - 1;
+
+    while d % 2 == 0 {
+        s += 1;
+        d >>= 1;
+    }
+
+    let check_list: Vec<i128> = vec![2, 325, 9375, 28178, 450775, 9780504, 1795265022];
+    for p in check_list {
+        if p > n {
+            break;
+        }
+
+        let mut t = 0;
+        let mut x = mod_pow(p, d, n);
+
+        if x == 1 {
+            continue;
+        }
+
+        while t < s {
+            if x == n - 1 {
+                break;
+            }
+
+            x = (x * x) % n;
+            t += 1;
+        }
+
+        if t == s {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::check_prime;
+    #[test]
+    fn check_prime_test() {
+        assert_eq!(check_prime(2), true);
+        assert_eq!(check_prime(341), false);
+        assert_eq!(check_prime(998244353), true);
+    }
+}
+
 fn main() {
     let args = Args::parse();
     let mut rng = rand::rng();
